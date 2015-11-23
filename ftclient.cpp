4e5cf7618ea051973cacc8bab8c99e06d3d53c92
Diga Widyaprana <digawp@gmail.com>
2015-11-23 21:03:04 +0800

    Fix indentations, newline, trailing whitespaces, and other minor refactorings

--- ftclient.cpp
+++ ftclient.cpp
@@ -8,17 +8,19 @@
 #include <sstream>
 
 using boost::asio::ip::tcp;
+
 class async_tcp_client
 {
 public:
     async_tcp_client(boost::asio::io_service& io_service,
         const std::string& server, const std::string& path)
-        : resolver_(io_service),
-        socket_(io_service)
+    : resolver_(io_service),
+    socket_(io_service)
     {
-        size_t pos = server.find(':'); 
-        if (pos==std::string::npos)
-            return;
+        size_t pos = server.find(':');
+        // no information regarding port number. Terminate.
+        if (pos == std::string::npos) return;
+
         std::string port_string = server.substr(pos+1);
         std::string server_ip_or_host = server.substr(0, pos);
 
@@ -28,26 +30,34 @@ public:
             std::cout << "failed to open " << path << std::endl;
             return ;
         }
+
         size_t file_size = source_file.tellg();
         source_file.seekg(0);
 
         std::cout << "File size: " << file_size << std::endl;
 
         // first send file name and file size to server
-        std::ostream request_stream(&request_);
+        std::ostream request_stream(&request_buf);
         request_stream << path << "\n"
             << file_size << "\n\n";
-        std::cout << "request size:" << request_.size() << std::endl;
+        std::cout << "request size:" << request_buf.size() << std::endl;
+
         // Start an asynchronous resolve to translate the server and service names
         // into a list of endpoints.
         tcp::resolver::query query(server_ip_or_host, port_string);
         resolver_.async_resolve(query,
             boost::bind(&async_tcp_client::handle_resolve, this,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::iterator));
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::iterator));
     }
 
 private:
+    tcp::resolver resolver_;
+    tcp::socket socket_;
+    boost::array<char, 1024> buf;
+    boost::asio::streambuf request_buf;
+    std::ifstream source_file;
+
     void handle_resolve(const boost::system::error_code& err,
         tcp::resolver::iterator endpoint_iterator)
     {
@@ -58,7 +68,7 @@ private:
             tcp::endpoint endpoint = *endpoint_iterator;
             socket_.async_connect(endpoint,
                 boost::bind(&async_tcp_client::handle_connect, this,
-                boost::asio::placeholders::error, ++endpoint_iterator));
+                    boost::asio::placeholders::error, ++endpoint_iterator));
         }
         else
         {
@@ -72,9 +82,9 @@ private:
         if (!err)
         {
             // The connection was successful. Send the request.
-            boost::asio::async_write(socket_, request_,
+            boost::asio::async_write(socket_, request_buf,
                 boost::bind(&async_tcp_client::handle_write_file, this,
-                boost::asio::placeholders::error));
+                    boost::asio::placeholders::error));
         }
         else if (endpoint_iterator != tcp::resolver::iterator())
         {
@@ -83,7 +93,7 @@ private:
             tcp::endpoint endpoint = *endpoint_iterator;
             socket_.async_connect(endpoint,
                 boost::bind(&async_tcp_client::handle_connect, this,
-                boost::asio::placeholders::error, ++endpoint_iterator));
+                    boost::asio::placeholders::error, ++endpoint_iterator));
         }
         else
         {
@@ -95,49 +105,51 @@ private:
     {
         if (!err)
         {
-                if (source_file.eof()==false)
+            if (source_file.eof()==false)
+            {
+                source_file.read(buf.c_array(), (std::streamsize)buf.size());
+                if (source_file.gcount()<=0)
                 {
-                    source_file.read(buf.c_array(), (std::streamsize)buf.size());
-                    if (source_file.gcount()<=0)
-                    {
-                        std::cout << "read file error " << std::endl;
-                        std::cout << "gcount: " << source_file.gcount() << std::endl;
-                        return;
-                    }
-                    std::cout << "send " <<source_file.gcount()<<" bytes, total:" << source_file.tellg() << " bytes.\n";
-                    boost::asio::async_write(socket_, 
-                        boost::asio::buffer(buf.c_array(), source_file.gcount()),
-                        boost::bind(&async_tcp_client::handle_write_file, this,
-                        boost::asio::placeholders::error));
-                    if (err)
-                    {
-                        std::cout << "send error:" << err << std::endl;
-                        return;
-                    }
-                }
-                else
+                    std::cout << "read file error " << std::endl;
+                    std::cout << "gcount: " << source_file.gcount() << std::endl;
                     return;
+                }
+                std::cout << "send " <<source_file.gcount()<<" bytes, total:" << source_file.tellg() << " bytes.\n";
+                boost::asio::async_write(socket_,
+                    boost::asio::buffer(buf.c_array(), source_file.gcount()),
+                    boost::bind(&async_tcp_client::handle_write_file, this,
+                        boost::asio::placeholders::error));
+                if (err)
+                {
+                    std::cout << "send error:" << err << std::endl;
+                    return;
+                }
+            }
+            else
+                return;
         }
         else
         {
             std::cout << "Error: " << err.message() << "\n";
         }
-
     }
-    tcp::resolver resolver_;
-    tcp::socket socket_;
-    boost::array<char, 1024> buf;
-    boost::asio::streambuf request_;
-    std::ifstream source_file;
 };
+
 int main(int argc, char* argv[])
 {
     if (argc != 3)
     {
         std::cerr << "Usage: " << argv[0] << " <server-address> <file path>" << std::endl;
+
+        #ifdef _WIN32
         std::cerr << "sample: " << argv[0] << " 127.0.0.1:1234 c:\\tmp\\a.txt" << std::endl;
+        #else
+        std::cerr << "sample: " << argv[0] << " 127.0.0.1:1234 c:/tmp/a.txt" << std::endl;
+        #endif
+
         return __LINE__;
     }
+
     try
     {
         boost::asio::io_service io_service;
@@ -145,7 +157,6 @@ int main(int argc, char* argv[])
         io_service.run();
 
         std::cout << "send file " << argv[2] << " completed successfully.\n";
-//         system("pause");
     }
     catch (std::exception& e)
     {
